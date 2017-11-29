85264f5643f3dcad529ab98a1b8f9d7eeccd5ada
Josef Kolář <thejoeejoee@gmail.com>
2017-11-29 11:41:56 +0100

    Fix OpenGL on debian based OS with nVidia #60

--- ide.py
+++ ide.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
+import sys
+
+if 'linux' in sys.platform.lower():
+    # noinspection PyUnresolvedReferences
+    from OpenGL import GL  # noqa
+
 from ifj2017.ide.main import main
 
 if __name__ == '__main__':
